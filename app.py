0b53a699c8eb561a40b55ada25ad607595a239d9
Todd-bit <todd7385@gmail.com>
2020-11-29 16:51:57 +0000

    Wired up edit function

--- app.py
+++ app.py
@@ -43,6 +43,26 @@ def add_task():
 
 
 
+@app.route ("/edit_task/<task_id>", methods=["GET", "POST"])
+def edit_task(task_id):
+    if request.method == "POST":
+        submit = {
+            "category_name": request.form.get("category_name"),
+            "task_name": request.form.get("task_name"),
+            "task_description": request.form.get("task_description")
+        }
+        mongo.db.tasks.update({"_id": ObjectId(task_id)}, submit)
+        flash ("Task Sucessfully Updated")    
+
+
+    
+    task = mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
+    categories = mongo.db.categories.find().sort("category_name", 1)   
+    return render_template("edit_task.html", task=task, categories=categories)    
+
+
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
