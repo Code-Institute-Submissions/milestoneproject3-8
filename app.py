df26998c7879820e093c78ed8e1bb76124cc5a61
Todd-bit <todd7385@gmail.com>
2020-12-05 20:29:33 +0000

    Added search bar

--- app.py
+++ app.py
@@ -22,7 +22,14 @@ mongo = PyMongo(app)
 def get_tasks():
     tasks = list(mongo.db.tasks.find())
     return render_template("tasks.html", tasks=tasks)
-    
+
+
+@app.route ("/search", methods=["GET", "POST"])   
+def search():
+    query=request.form.get("query")
+    tasks = list(mongo.db.tasks.find({"$text": {"$search": query}}))
+    return render_template("tasks.html", tasks=tasks)
+
 
 @app.route("/add_task", methods = ["GET", "POST"]) 
 def add_task():
