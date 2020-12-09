a5805b164b3548f524e3c3f40b4302256debe4b2
Todd-bit <todd7385@gmail.com>
2020-12-09 23:09:25 +0000

    Changed pictures on carousel

--- static/js/script.js
+++ static/js/script.js
@@ -2,4 +2,7 @@ $(document).ready(function(){
     $('.collapsible').collapsible();
     $("select").formSelect();
     $('.carousel').carousel();
+    setInterval(function() {
+        $(".carousel").carousel("next");
+    }, 3000);
 });
