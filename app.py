9a50a29cc25b3a7a3e98a53f33f55571a1445a5f
Todd-bit <todd7385@gmail.com>
2021-01-29 00:28:14 +0000

    Changed the db

--- app.py
+++ app.py
@@ -20,14 +20,14 @@ mongo = PyMongo(app)
 @app.route("/")
 @app.route("/get_tasks")
 def get_tasks():
-    tasks = list(mongo.db.tasks.find())
+    tasks = list(mongo.db.recipe.find())
     return render_template("tasks.html", tasks=tasks)
 
 
 @app.route ("/search", methods=["GET", "POST"])   
 def search():
     query=request.form.get("query")
-    tasks = list(mongo.db.tasks.find({"$text": {"$search": query}}))
+    tasks = list(mongo.db.recipe.find({"$text": {"$search": query}}))
     return render_template("tasks.html", tasks=tasks)
 
 
@@ -40,11 +40,11 @@ def add_task():
             "ingredients": request.form.get("ingredients"),
             "prep": request.form.get("prep")
         }
-        mongo.db.tasks.insert_one(task)
+        mongo.db.recipe.insert_one(task)
         flash ("Recipe Added!") 
         return redirect(url_for("get_tasks"))  
 
-    categories = mongo.db.categories.find().sort("course", 1)
+    categories = mongo.db.course.find().sort("course", 1)
     return render_template("add_task.html", categories=categories)  
 
 
@@ -59,20 +59,20 @@ def edit_task(task_id):
             "ingredients": request.form.get("ingredients"),
             "prep": request.form.get("prep")
         }
-        mongo.db.tasks.update({"_id": ObjectId(task_id)}, submit)
+        mongo.db.recipe.update({"_id": ObjectId(task_id)}, submit)
         flash ("Recipe Updated!")    
 
 
     
-    task = mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
-    categories = mongo.db.categories.find().sort("dish", 1)   
+    task = mongo.db.recipe.find_one({"_id": ObjectId(task_id)})
+    categories = mongo.db.course.find().sort("dish", 1)   
     return render_template("edit_task.html", task=task, categories=categories)    
 
 
 
 @app.route ("/delete_task/<task_id>")
 def delete_task(task_id):
-    mongo.db.tasks.remove({"_id": ObjectId(task_id)})
+    mongo.db.recipe.remove({"_id": ObjectId(task_id)})
     flash ("Recipe Deleted!")
     return redirect(url_for("get_tasks"))    
 
